269c1924e5ddb57f54151882b635a00c334734f4
Imran Imtiaz <imranimtiaz48@hotmail.com>
2024-07-25 08:35:35 +0400

    Update 1321_Restaurant_Growth.sql

--- OFFSET-FETCH/1321_Restaurant_Growth.sql
+++ OFFSET-FETCH/1321_Restaurant_Growth.sql
@@ -1,15 +1,21 @@
-SELECT visited_on, 
+-- Calculate the rolling sum and average amount over a 7-day window
+SELECT 
+    visited_on,
+    
+    -- Compute the rolling sum of 'amount' over the past 7 days including the current day
     SUM(SUM(amount)) OVER (
         ORDER BY visited_on 
         ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
     ) AS amount,
+    
+    -- Compute the rolling average of 'amount' over the past 7 days including the current day
     CAST(
         SUM(SUM(amount)) OVER (
             ORDER BY visited_on 
             ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
-        )/7.0
-        AS  DECIMAL(10,2)
-    )  AS average_amount
+        ) / 7.0
+        AS DECIMAL(10, 2)
+    ) AS average_amount
 FROM Customer
 GROUP BY visited_on
 ORDER BY visited_on
