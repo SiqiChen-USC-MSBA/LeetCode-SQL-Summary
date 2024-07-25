8f83d5c4e04832473380b996fd321d7836a7a181
Imran Imtiaz <imranimtiaz48@hotmail.com>
2024-07-25 08:38:39 +0400

    Update 602_Friend_Requests_II_Who_Has_the_Most_Friends.sql

--- PIVOT-UNPIVOT/602_Friend_Requests_II_Who_Has_the_Most_Friends.sql
+++ PIVOT-UNPIVOT/602_Friend_Requests_II_Who_Has_the_Most_Friends.sql
@@ -1,7 +1,13 @@
-SELECT TOP 1 upvt.id, COUNT(*) AS num
+-- Retrieve the ID with the highest count of occurrences from requester_id and accepter_id
+SELECT TOP 1 
+    upvt.id, 
+    COUNT(*) AS num
 FROM request_accepted
+-- Unpivot the columns requester_id and accepter_id into a single column 'id'
 UNPIVOT (
     id FOR ids IN (requester_id, accepter_id)
-) upvt
+) AS upvt
+-- Group by the unpivoted ID to get the count
 GROUP BY upvt.id
+-- Order by count in descending order to get the most frequent ID
 ORDER BY COUNT(*) DESC;
